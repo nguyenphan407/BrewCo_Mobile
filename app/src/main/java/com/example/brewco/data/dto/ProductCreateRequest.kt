a53f7c41cb44892c29c382ae4dcfba2a6c611ef6
Quan-Vo-Dinh <vodinhquan2707.it@gmail.com>
2026-01-10 01:57:14 +0700

    chore(data): port product and category dtos

---
+++ app/src/main/java/com/example/brewco/data/dto/ProductCreateRequest.kt
@@ -0,0 +1,17 @@
+package com.example.brewco.data.dto
+package com.example.brewco.data.dto
+
+data class ProductCreateRequest (
+    val name: String,
+    val description: String,
+    val price: Int,
+    val imageUrl: String,
+    val categoryId: Int
+    )
+data class ProductCreateRequest (
+    val name: String,
+    val description: String,
+    val price: Int,
+    val imageUrl: String,
+    val categoryId: Int
+    )
